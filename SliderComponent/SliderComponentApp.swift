acba58b6a10d8423b716439528f232f402980899
ALX <alexmonac13@gmail.com>
2023-07-18 00:02:59 +0200

    Initial Commit

---
+++ SliderComponent/SliderComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderComponentApp.swift
+//  SliderComponent
+//
+//  Created by Alex on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
