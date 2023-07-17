4b8f807d1d23e2bfd2bff294fdbc02bae1a38058
ALX <alexmonac13@gmail.com>
2023-07-18 00:56:04 +0200

    SwiftUI Sliders state

--- SliderComponent/ContentView.swift
+++ SliderComponent/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var value: Double = 0
+    @State private var value2: Double = 50
+    @State private var sliderValue: Double = 1
+    @State private var step: Double = 10
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form{
+
+            // Premier slider
+            Section {
+                Text("Slider avec plage = \(value)")
+                
+                Slider(value: $value, in: 0...100).accentColor(.green)
+            }
+            .padding(.all)
+            
+            // Second slider
+            Section {
+                Text("Slider avec un pas de \(Int(step)) = \(Int(value2))")
+                
+                Slider(value: $value2, in: 0...100, step: step).accentColor(.red)
+            }
+            .padding(.all)
+            
+            // Troisieme slider
+            Section {
+                Text("Nombre de personnes : \(Int(sliderValue))")
+                
+                Slider(value: $sliderValue, in: 0...6, step: 1, onEditingChanged: {(sliderValue) in print(sliderValue)}, minimumValueLabel: Image(systemName: "person.fill"),maximumValueLabel: Image(systemName: "person.3.fill"), label: {
+                    Text("Label")
+                }).accentColor(.blue)
+            }
+            .padding(.all)
         }
-        .padding()
     }
 }
 
